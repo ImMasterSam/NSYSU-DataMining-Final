90af1d3b3b2dbec6f446e986b42db80c715a2bf7
ImMasterSam <sam9630147@gmail.com>
2025-05-29 11:40:58 +0800

    Add probability to SVM

--- Classifiers/SVM.py
+++ Classifiers/SVM.py
@@ -4,9 +4,9 @@ from Classifiers.classifier import Classifier
 from sklearn.svm import SVC
 
 class SVMClassifier(Classifier):
-    def __init__(self, kernel: str = "rbf", normalize: bool = True):
-        super().__init__(f'Kernel SVM ({kernel}) Classifier', normalize)
-        self.clf = SVC(kernel = kernel)
+    def __init__(self, kernel: str = "rbf", normalize: bool = True, proba: bool = False, threshold: float = 0.6):
+        super().__init__(f'Kernel SVM ({kernel}) Classifier', normalize, proba, threshold)
+        self.clf = SVC(kernel = kernel, probability = proba)
 
     def fit(self, x_train: pd.DataFrame, y_train: pd.Series):
         x_train = x_train.astype(float).to_numpy()
@@ -23,6 +23,16 @@ class SVMClassifier(Classifier):
         if self.normalize:
             x_test = self.scaler.transform(x_test)
 
-        y_predict = self.clf.predict(x_test)
+        if self.proba:
+            y_proba = self.clf.predict_proba(x_test)
+
+            # 找出最大機率的類別及其機率
+            max_proba = np.max(y_proba, axis=1)
+            max_class_idx = np.argmax(y_proba, axis=1)
+            # 只保留機率大於 threshold 的標籤，否則設為 None 或其他標記
+            y_predict = np.where(max_proba >= self.threshold,
+                                 self.clf.classes_[max_class_idx], -1)
+        else:
+            y_predict = self.clf.predict(x_test)
 
         return y_predict
