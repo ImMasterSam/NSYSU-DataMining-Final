8c3d23828a220c50b46d7efe4b2602cdafc0c17b
ImMasterSam <sam9630147@gmail.com>
2025-05-29 11:35:44 +0800

    Add probability to RandomF

--- Classifiers/RandomF.py
+++ Classifiers/RandomF.py
@@ -4,8 +4,8 @@ from Classifiers.classifier import Classifier
 from sklearn.ensemble import RandomForestClassifier as skRandomForest
 
 class RandomForestClassifier(Classifier):
-    def __init__(self, n_estimators: int = 10, max_depth: int = 10, min_samples_split: int = 2, normalize: bool = True):
-        super().__init__('Random Forest Classifier', normalize)
+    def __init__(self, n_estimators: int = 10, max_depth: int = 10, min_samples_split: int = 2, normalize: bool = True, proba: bool = False, threshold: float = 0.6):
+        super().__init__('Random Forest Classifier', normalize, proba, threshold)
         self.clf = skRandomForest(n_estimators = n_estimators, max_depth = max_depth, min_samples_split = min_samples_split)
 
     def fit(self, x_train: pd.DataFrame, y_train: pd.Series):
@@ -23,6 +23,16 @@ class RandomForestClassifier(Classifier):
         if self.normalize:
             x_test = self.scaler.transform(x_test)
 
-        y_predict = self.clf.predict(x_test)
+        if self.proba:
+            y_proba = self.clf.predict_proba(x_test)
+
+            # 找出最大機率的類別及其機率
+            max_proba = np.max(y_proba, axis=1)
+            max_class_idx = np.argmax(y_proba, axis=1)
+            # 只保留機率大於 threshold 的標籤，否則設為 None 或其他標記
+            y_predict = np.where(max_proba >= self.threshold,
+                                 self.clf.classes_[max_class_idx], -1)
+        else:
+            y_predict = self.clf.predict(x_test)
         
         return y_predict
--- args.py
+++ args.py
@@ -7,7 +7,7 @@ models = ('K Nearest Neighbors', 'Neural Network', 'Random Forest', 'Kernel SVM 
 
 model_options = {'K Nearest Neighbors' : KNNClassifier(k = 3, normDistance = 2, proba = True, threshold = 0.7),
                  'Neural Network' : NeuralNetClassifier(n_hidden = 10, learning_rate = 0.001, n_iters = 1000, proba = True, threshold = 0.6),
-                 'Random Forest' : RandomForestClassifier(n_estimators = 10, max_depth = 10, min_samples_split = 2),
+                 'Random Forest' : RandomForestClassifier(n_estimators = 10, max_depth = 10, min_samples_split = 2, proba = True, threshold = 0.6),
                  'Kernel SVM (linear)' : SVMClassifier(kernel = 'linear'),
                  'Kernel SVM (poly)' : SVMClassifier(kernel = 'poly'),
                  'Kernel SVM (rbf)' : SVMClassifier(kernel = 'rbf'),
